952e84cab969ae94332be69ebd6a091b8b46817f
roxerj <juozapas.c@gmail.com>
2023-05-15 18:32:00 +0300

    testing

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # testing myVector class against std::vector
 
+
+Testing some myVector functions against std::vector functions
+|**container\function**   |emplace_back(1,2,3)   |resize(2)   |1M   |10M   |100M   |
+|---|---|---|---|---|---|
+|**std::vector**   |_elements after function: 1, 2, 3_   |_size after function: 2_   |_0.002321s_   |_0.030825s_   |_0.278785s_   |
+|**myVector**   |_elements after function: 1, 2, 3_   |_size after function: 2_   |_0.00208s_   |_0.026683s_  |_0.214362s_   |
+
+
+
+
+
+
+
 Testing how much time it takes to fill a container with integers:
-|**container/size**   |10k   |100k   |1M   |10M   |100M   |
+|**container\size**   |10k   |100k   |1M   |10M   |100M   |
 |---|---|---|---|---|---|
 |**std::vector**   |_5.4e-05s_   |_0.000475s_   |_0.002321s_   |_0.030825s_   |_0.278785s_   |
 |**myVector**   |_3.9e-05s_   |_0.000412s_   |_0.00208s_   |_0.026683s_  |_0.214362s_   |
--- main.cpp
+++ main.cpp
@@ -5,31 +5,51 @@ using namespace std;
 int main()
 { 
     // testing myVector class
-    unsigned int sz = 0;  // 100000, 1000000, 10000000, 100000000
-    std::cout << "Enter size of container: ";
-    std::cin >> sz;
     
-
-    Timer stdVecTimer;
-    stdVecTimer.start();
-
     std::vector<int> stdVec;
-    for (int i = 1; i <= sz; ++i)
-    {
-        stdVec.push_back(i);
-    }
-    stdVecTimer.stop();
-
-    Timer myVecTimer;
-    myVecTimer.start();
     myVector<int> myVec;
-    for (int i = 1; i <= sz; ++i)
-    {
-        myVec.push_back(i);
-    }
-    myVecTimer.stop();
 
-    std::cout << "std::vector: " << stdVecTimer.elapsed() << " sec." << std::endl;
-    std::cout << "myVector: " << myVecTimer.elapsed() << " sec." << std::endl;
+    stdVec.emplace_back(1, 2, 3);
+
+    myVec.emplace_back(1, 2, 3);
+
+    for(int i = 0; i < stdVec.size(); i++)
+    {
+        std::cout << stdVec[i] << " ";
+    }
+    std::cout << std::endl;
+    myVec.print();
+
+
+
+
+
+
+    // unsigned int sz = 0;  // 100000, 1000000, 10000000, 100000000
+    // std::cout << "Enter size of container: ";
+    // std::cin >> sz;
+
+
+    // Timer stdVecTimer;
+    // stdVecTimer.start();
+
+
+    // for (int i = 1; i <= sz; ++i)
+    // {
+    //     stdVec.push_back(i);
+    // }
+    // stdVecTimer.stop();
+
+    // Timer myVecTimer;
+    // myVecTimer.start();
+    // for (int i = 1; i <= sz; ++i)
+    // {
+    //     myVec.push_back(i);
+    // }
+    // myVecTimer.stop();
+
+    // std::cout << "std::vector: " << stdVecTimer.elapsed() << " sec." << std::endl;
+    // std::cout << "myVector: " << myVecTimer.elapsed() << " sec." << std::endl; 
+
 
 }
